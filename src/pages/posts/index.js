6991d4ef4d5fad7f8f3fca19a524b2e0847c6e26
Tarek-Mdimagh <tarekmdemegh@gmail.com>
2022-05-15 04:13:17 +0100

    move folder componet out of pages

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 
 import { Link } from 'react-router-dom'
-import RecipeReviewCard from '../components/card'
+import RecipeReviewCard from '../../components/card'
 
 function Posts() {
   return (
